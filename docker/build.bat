6fdef5d676a0e5cb253dcf2e990b17ab1888ee79
Dara Keon <lc.sales.duarte@gmail.com>
2021-02-17 10:45:41 -0300

    Ignore generated files into docker copy

--- docker/build.bat
+++ docker/build.bat
@@ -1,16 +1,15 @@
 @echo off
 
-docker context use default
+set dir=%~dp0
+set push=%~1
+
+docker context use default > NUL
 
 docker stop mebrak > NUL 2> NUL
 docker rm mebrak > NUL 2> NUL
 
-cd ..
+docker build .. -t darakeon/mebrak -f "%dir%mebrak.dockerfile"
 
-copy docker\mebrak.dockerfile mebrak.dockerfile > NUL
-docker build . -f mebrak.dockerfile -t darakeon/mebrak
-del mebrak.dockerfile > NUL
-
-cd docker
-
-docker push darakeon/mebrak
+if "%push%" neq "" (
+	docker %push% darakeon/mebrak
+)
